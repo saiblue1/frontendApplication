f4c7fc3a086ad6c5bf2b66319087cb9357b9076a
saiblue1 <112603259+saiblue1@users.noreply.github.com>
2022-09-01 09:32:01 +0530

    Update HelloResource.java

--- src/main/java/frontendjava/frontendApplication/HelloResource.java
+++ src/main/java/frontendjava/frontendApplication/HelloResource.java
@@ -1,15 +1,21 @@
-package frontendjava.frontendApplication;
+package com.javatechie.k8s;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
 @RestController
-@RequestMapping("/rest/docker/hello")
-public class HelloResource {
+public class SpringbootK8sDemoApplication {
 
-    @GetMapping
-    public String hello() {
-        return "Hello BlueBinaries";
-    }
+	@GetMapping("/message")
+	public String displayMessage(){
+		return "Congratulation you successfully deployed your application to kubernetes !!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootK8sDemoApplication.class, args);
+	}
+
 }
